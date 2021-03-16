2be24751327052266e9cb58067e91e74338518b6
wezrine@me.com <williamezrine@williams-mbp.lan>
2021-03-16 17:06:03 -0400

    can delete trips

--- Trips/app.js
+++ Trips/app.js
@@ -7,10 +7,12 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.use(express.urlencoded())
+const { v4: uuidv4 } = require('uuid');
+app.use(express.static('css'))
 
 let trips = [
-    {title: 'Africa', departureDate: '03/16/21', returnDate: '04/16/21', imgURL: 'https://images.unsplash.com/photo-1516026672322-bc52d61a55d5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YWZyaWNhfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=60'},
-    {title: 'Ireland', departureDate: '03/22/21', returnDate: '04/22/21', imgURL: 'https://images.unsplash.com/photo-1451337516015-6b6e9a44a8a3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aXJlbGFuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=60'}
+    {title: 'Africa', departureDate: '03/16/21', returnDate: '04/16/21', imgURL: 'https://images.unsplash.com/photo-1516026672322-bc52d61a55d5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YWZyaWNhfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=60', tripId: uuidv4()},
+    {title: 'Ireland', departureDate: '03/22/21', returnDate: '04/22/21', imgURL: 'https://images.unsplash.com/photo-1451337516015-6b6e9a44a8a3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aXJlbGFuZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=60', tripId: uuidv4()}
 ]
 
 app.get('/trips', (req, res) => {
@@ -31,9 +33,17 @@ app.post('/create-trip', (req, res) => {
     trips.push(trip)
 
     res.redirect('/trips')
-
 })
 
+app.post('/delete-trip', (req, res) => {
+    const tripId = req.body.tripId
+
+    trips = trips.filter((trip) => {
+        return trip.tripId != tripId
+    })
+
+    res.redirect('/trips')
+})
 app.listen(3000, () => {
     console.log('Server is running...')
 })
